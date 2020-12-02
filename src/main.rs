a1f95f386b29b0cc5bc705e5314fd1c18597cfe2
Laurențiu Nicola <lnicola@dend.ro>
2020-12-02 09:53:05 +0200

    Add day 2

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 use anyhow::{anyhow, bail, Result};
+use serde_scan::scan;
 
 fn day1() -> Result<()> {
     let file = File::open("day1.txt")?;
@@ -28,7 +29,7 @@ fn day1() -> Result<()> {
     }
 
     let (i, j) = sum2(xs.as_slice(), 2020).ok_or_else(|| anyhow!("no solution"))?;
-    println!("{}", xs[i] * xs[j]);
+    print!("{} ", xs[i] * xs[j]);
 
     for i in 0..xs.len() - 2 {
         if let Some((j, k)) = sum2(&xs[i + 1..], 2020 - xs[i]) {
@@ -40,6 +41,29 @@ fn day1() -> Result<()> {
     bail!("no solution")
 }
 
+fn day2() -> Result<()> {
+    let file = File::open("day2.txt")?;
+    let reader = BufReader::new(file);
+    let mut valid1 = 0;
+    let mut valid2 = 0;
+    for line in reader.lines() {
+        let line = &line?;
+        let (pos1, pos2, ch, password): (_, _, char, &str) = scan!("{}-{} {}: {}" <- line)?;
+        let password = password.as_bytes();
+        let ch = ch as u8;
+        let count = password.iter().filter(|&&c| c == ch).count();
+        if count >= pos1 && count <= pos2 {
+            valid1 += 1;
+        }
+        if (password[pos1 - 1] == ch) ^ (password[pos2 - 1] == ch) {
+            valid2 += 1;
+        }
+    }
+    println!("{} {}", valid1, valid2);
+    Ok(())
+}
+
 fn main() -> Result<()> {
-    day1()
+    day1()?;
+    day2()
 }
